87042ec30f3cc18dabd03ff685c2f66069f5d77d
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-09-20 11:53:51 +0600

    Setup all thing

---
+++ models/tourManagement.modle.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const toureSchema = mongoose.Schema({
+       
+})
+  //model
+  const toureManagement = mongoose.model('Products', productSchema)
+
+  module.exports=toureManagement;
